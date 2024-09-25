0762a90472d0f11beb80aa35e1e95a87705a055b
Lucas <lucaslbmp@github.com>
2024-09-25 15:09:45 -0300

    Adding colors and gradient

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,11 @@ const config: Config = {
         border: "var(--border-color)",
         textPrimary: "var(--text-primary-color)",
         textSecondary: "var(--text-secondary-color)",
+        bgHighlight: "var(--bg-highlight)",
       },
+      backgroundImage: () => ({
+        "gradient-primary": `linear-gradient(to right, #F1C090 0%, 39.69258666038513%, #C07FB3 79.38517332077026%, 89.69258666038513%, #AB4199 100%)`,
+      }),
     },
   },
   plugins: [],
