4330420339196ade77015f58681302a2aed35de0
Lucas <lucaslbmp@github.com>
2024-09-25 15:25:24 -0300

    Adding header to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Header from "./_components/header";
 import SearchForm from "./_components/search-form";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-items-center gap-4 min-h-screen font-[family-name:var(--font-geist-sans)]">
+      <Header />
       <main className="flex bg-[url('/hero-image.png')] bg-center w-full h-auto min-h-screen justify-center flex-1 items-center">
         <SearchForm />
       </main>
