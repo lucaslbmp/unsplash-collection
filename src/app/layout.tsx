5e33da748b5a505eb5d9a5219488ac855f4a4d55
Lucas <lucaslbmp@github.com>
2024-09-25 15:12:07 -0300

    Adding max width to body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} antialiased text-base text-textPrimary`}
+        className={`${inter.className} antialiased text-base text-textPrimary max-w-vw`}
       >
         {children}
       </body>
